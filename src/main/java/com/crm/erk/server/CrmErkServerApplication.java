cf5ba74d10a3c3374d3833471e096ed790d0ff9b
hicham <sass.hicham@gmail.com>
2020-12-12 23:36:39 +0100

    init

---
+++ src/main/java/com/crm/erk/server/CrmErkServerApplication.java
@@ -0,0 +1,15 @@
+package com.crm.erk.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CrmErkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmErkServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/erk/server/CrmErkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.erk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmErkServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
